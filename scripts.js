56b5ea62eb38eba313ff5b0f00422fdd0c47d96e
HelloGonzo <118839950+HelloGonzo@users.noreply.github.com>
2023-07-16 19:19:03 -0400

    mobile nav complete

--- scripts.js
+++ scripts.js
@@ -79,7 +79,34 @@ function handleTouchMove(evt) {
 };
 
 
-
 // Mobile Nav
 
-const menu = document.querySelector('')
+const menu = document.querySelector('header img'),
+      header = document.querySelector('header'),
+      body = document.querySelector('body'),
+      navItems = Array.from(document.querySelectorAll('header a'));
+
+
+function toggleNav() {
+    header.classList.toggle('open')
+    if (header.classList.contains('open')) {
+        menu.src = './assets/close.png'
+        window.onresize = closeNav
+        body.style.overflowY = 'hidden';
+        navItems.forEach(item => {
+            item.onclick = closeNav;
+        })
+    } else {
+        menu.src = './assets/menu.png'
+        body.style.overflowY = 'auto';
+    }
+}
+
+function closeNav() {
+    header.classList.remove('open');
+    menu.src = './assets/menu.png'
+    body.style.overflowY = 'auto';
+}
+
+menu.onclick = toggleNav;
+
