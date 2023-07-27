d3819cd8381ec00f74a022cd5014d045345756de
HelloGonzo <118839950+HelloGonzo@users.noreply.github.com>
2023-07-26 20:49:16 -0400

    updated content, fixed links

--- mobileNav.js
+++ mobileNav.js
@@ -10,21 +10,21 @@ const menu = document.querySelector('header img'),
 function toggleNav() {
     header.classList.toggle('open')
     if (header.classList.contains('open')) {
-        menu.src = './assets/close.png'
+        menu.src = '../assets/close.png'
         window.onresize = closeNav
         body.style.overflowY = 'hidden';
         navItems.forEach(item => {
             item.onclick = closeNav;
         })
     } else {
-        menu.src = './assets/menu.png'
+        menu.src = '../assets/menu.png'
         body.style.overflowY = 'auto';
     }
 }
 
 function closeNav() {
     header.classList.remove('open');
-    menu.src = './assets/menu.png'
+    menu.src = '../assets/menu.png'
     body.style.overflowY = 'auto';
 }
 
